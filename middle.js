4fb1e053f38b51f7c2d829f88f9a467c1adafd87
wentingzoe <ywtzoe@gmail.com>
2021-11-24 14:13:05 -0500

    edited the assert arrary funtion to test

--- middle.js
+++ middle.js
@@ -34,9 +34,9 @@ const middle = function(arr) {
 	}
 	return newarr;
 };
-console.log(middle([ 1 ]));
-console.log(middle([ 1, 2 ]));
-console.log(middle([ 1, 2, 3 ]));
-console.log(middle([ 1, 2, 3, 4, 5 ]));
-console.log(middle([ 1, 2, 3, 4 ]));
-console.log(middle([ 1, 2, 3, 4, 5, 6 ]));
+console.log(assertArraysEqual(middle([ 1 ]), []));
+console.log(assertArraysEqual(middle([ 1, 2 ]), []));
+console.log(assertArraysEqual(middle([ 1, 2, 3 ]), [ 2 ]));
+console.log(assertArraysEqual(middle([ 1, 2, 3, 4, 5 ]), [ 3 ]));
+console.log(assertArraysEqual(middle([ 1, 2, 3, 4 ]), [ 2, 3 ]));
+console.log(assertArraysEqual(middle([ 1, 2, 3, 4, 5, 6 ]), [ 3, 4 ]));
