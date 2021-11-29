841162aaa367b4397b755618464a66e75ca62b1c
wentingzoe <ywtzoe@gmail.com>
2021-11-29 13:05:27 -0500

    edite assertArraysEqual.js to module style

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (Array.isArray(arr1) && Array.isArray(arr2)) {
-    if (arr1.length === arr2.length) {
-      for (let i in arr1) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  } else {
-    return false;
-  }
- 
-};
+const eqArrays = require('../eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -20,3 +7,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
